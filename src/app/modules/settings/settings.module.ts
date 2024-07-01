99bfe53948c805d4b2017f5d76d3296ab72175c9
Omotosho Kamalideen <dapious99@gmail.com>
2024-07-01 19:16:54 +0100

    settings page done with little addition to be added

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -6,12 +6,16 @@ import { SharedModule } from '../../shared/shared.module';
 import { NotificationToggleComponent } from '../../notification-toggle/notification-toggle.component';
 import { SecurityComponent } from './security/security.component';
 import { PreferencesComponent } from './preferences/preferences.component';
+import { ButtonComponent } from '../../shared/button/button.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     SecurityComponent,
     PreferencesComponent,
+    ProfileComponent,
     NotificationToggleComponent,
+    ButtonComponent,
   ],
   imports: [CommonModule, SettingsRoutingModule, SharedModule],
 })
---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css'],
+})
+export class ButtonComponent {
+  @Input() label: string = 'Save';
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() disabled: boolean = false;
+  @Input() class: string = '';
+}
