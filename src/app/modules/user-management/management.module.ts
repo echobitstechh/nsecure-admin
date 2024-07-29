90359c3f18fb838d820cd65e2dbcfc9df75452e9
Omotosho Kamalideen <dapious99@gmail.com>
2024-07-29 17:58:27 +0100

    other inputs added for access level

--- src/app/modules/user-management/management.module.ts
+++ src/app/modules/user-management/management.module.ts
@@ -6,9 +6,10 @@ import { UsersComponent } from './users/users.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../../shared/shared.module';
+import { ManagementDetailsComponent } from './management-details/management-details.component';
 
 @NgModule({
-  declarations: [UsersComponent, AddUserComponent],
+  declarations: [UsersComponent, AddUserComponent, ManagementDetailsComponent],
   imports: [
     CommonModule,
     ManagementRoutingModule,
