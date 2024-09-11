7e0b6dac1128de9e1a662968400a5d6a00dd0b49
Kodewarlock <dapious99@gmail.com>
2024-09-11 11:13:41 +0100

    clean up

---
+++ src/app/modules/user-management/park-management/parkmanagement-page/parkmanagement-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parkmanagement-page',
+  templateUrl: './parkmanagement-page.component.html',
+  styleUrl: './parkmanagement-page.component.css',
+})
+export class ParkmanagementPageComponent {}
